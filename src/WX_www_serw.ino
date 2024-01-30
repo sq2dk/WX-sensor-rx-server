ff50f918f2c49def0401402101580615fc37d116
sq2dk <40606602+sq2dk@users.noreply.github.com>
2024-01-30 21:09:41 +0100

    Major updates. Calculating 5min average values

--- src/WX_www_serw.ino
+++ src/WX_www_serw.ino
@@ -7,7 +7,14 @@
 // and on:
 // https://esp32tutorials.com/esp32-bme280-web-server-esp-idf/ - if I remember correctly
 //
+//  TO DO - serial communication for data exchange
+//  TO DO - average values for last 5 minutes
+//  TO DO - variation of wind in time.
+//  TO DO - rain during last 60min
+//
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 #include <Arduino.h>
 #include "WeatherSensorCfg.h"
@@ -21,17 +28,24 @@
 //#define ESP8266
 
 // Replace with your network credentials
-const char* ssid = "YOUR_SSID";
-const char* password = "YOUR_PASSWD";
+//const char* ssid = "SP2ZIE";
+//const char* password = "cqcqcqdesp2zie";
+
+const char* ssid = "SSID";
+const char* password = "password";
+
+const char* station = "Your station";
 
 
 // Set your Static IP address
-IPAddress local_IP(10, 0, 0, 77);
+IPAddress local_IP(192, 168, 1, 2);
+
 // Set your Gateway IP address
-IPAddress gateway(10, 0, 0, 1);
+IPAddress gateway(192, 168, 1, 1);
 
 IPAddress subnet(255, 255, 255, 0);
-IPAddress primaryDNS(10, 0, 0, 1);   //optional
+
+IPAddress primaryDNS(8, 8, 8, 8);   //optional
 IPAddress secondaryDNS(8, 8, 4, 4); //optional
 
 
@@ -49,11 +63,39 @@ char incomingPacket[255];  // buffer for incoming packets
 #define TIMEOUT 45000 // Timeout in ms
 #define BRESSER_5_IN_1
 
+#define INT_TIME 300000  //integration time in ms (5min)
+//#define INT_TIME 100000  //integration time in ms (5min)
+
 AsyncWebServer server(80);
 AsyncEventSource events("/events");
 
+float deg_to_rad = 0.01745329252;  //deg to rad calc
+
 unsigned long lastTime = 0;  
+unsigned long lastDecoded = 0;
 unsigned long timerDelay = 30000;  // send readings timer
+
+float last_time;
+
+// avereage related 
+unsigned long last_integr = 0;  //time for chceking last integration (averege value calculation)
+unsigned long packet_no = 0;   //number of received packets in current integration
+
+float sum_temperature = 0;
+float sum_humidity = 0;
+double sum_x_wind = 0;  //for vector sum calculation
+double sum_y_wind = 0;  //for vector sum calculation
+float sum_wind_avr = 0;
+float max_wind_gust = 0;
+
+float avr_temperature;
+float avr_humidity;
+float avr_wind_dir;  //for vector sum calculation
+float avr_wind_avr;
+float avr_wind_gust;
+float avr_wind_consistency;
+unsigned long avr_number;
+
 
 float temperature;
 float humidity;
@@ -63,33 +105,52 @@ float wind_gust;
 float raindrop;
 float rssi;
 
+
+
 WeatherSensor ws;
+byte Verbose_level=0;
+byte avr_val_www = 1;  //1- show avereage values on WWW, 0 - sow current values
 
 
 String processor(const String& var){
   //getBME680Readings();
-  //Serial.println(var);
-  if(var == "TEMPERATURE"){
-    return String(ws.sensor[0].w.temp_c,1);
+  if (Verbose_level>4) {Serial.println(var);}
+  
+  if (avr_val_www) {
+    if(var == "TEMPERATURE") { return String(avr_temperature,1); }
+    else if(var == "HUMIDITY") { return String(avr_humidity,0); }
+    else if(var == "WIND_AVR") { return String(avr_wind_avr,1); }
+    else if(var == "WIND_DIR") { return String(avr_wind_dir,0); }
+    else if(var == "WIND_GUST") {return String(avr_wind_gust,1); }
   }
-  else if(var == "HUMIDITY"){
-    return String(ws.sensor[0].w.humidity);
+  else if (avr_val_www==0) {
+   if(var == "TEMPERATURE"){
+     return String(ws.sensor[0].w.temp_c,1);
+    }
+   else if(var == "HUMIDITY"){
+     return String(ws.sensor[0].w.humidity,0);
+    }
+   else if(var == "WIND_AVR"){
+     return String(ws.sensor[0].w.wind_avg_meter_sec,1);
+    }
+   else if(var == "WIND_DIR"){
+      return String(ws.sensor[0].w.wind_direction_deg,0);
+    }
+   else if(var == "WIND_GUST"){
+     return String(ws.sensor[0].w.wind_gust_meter_sec,1);
+    }
   }
-  else if(var == "WIND_AVR"){
-    return String(ws.sensor[0].w.wind_avg_meter_sec,1);
-  }
-  else if(var == "WIND_DIR"){
-    return String(ws.sensor[0].w.wind_direction_deg,0);
-  }
- else if(var == "WIND_GUST"){
-    return String(ws.sensor[0].w.wind_gust_meter_sec,1);
-  }
-   else if(var == "RAINDROP"){
+
+  if(var == "RAINDROP"){
     return String(ws.sensor[0].w.rain_mm,1);
   }
   else if(var == "RSSI"){
     return String(ws.sensor[0].rssi,0);
   }
+  else if(var == "STATION"){
+    return String(station);
+  }
+
 
   return String();
 }
@@ -97,7 +158,7 @@ String processor(const String& var){
 const char index_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
 <head>
-  <title>SQ2DK Meteo</title>
+  <title>%STATION% Meteo</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
   <link rel="icon" href="data:,">
@@ -121,7 +182,7 @@ const char index_html[] PROGMEM = R"rawliteral(
 </head>
 <body>
   <div class="topnav">
-    <h3>SQ2DK METEO</h3>
+    <h3>%STATION% METEO</h3>
   </div>
   <div class="content">
     <div class="cards">
@@ -294,8 +355,8 @@ void setup() {
   Serial.println(WiFi.localIP());
   Serial.println();
 
-
-    ws.begin();
+  if (Verbose_level>2) { Serial.printf("Starting radio interface\n");}
+  ws.begin();
 
       // Handle Web Server
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -305,7 +366,7 @@ void setup() {
   // Handle Web Server Events
   events.onConnect([](AsyncEventSourceClient *client){
     if(client->lastId()){
-      Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId());
+      if (Verbose_level>1) { Serial.printf("Client reconnected! Last message ID that it got is: %u\n", client->lastId()); }
     }
     // send event with message "hello!", id current millis
     // and set reconnect delay to 1 second
@@ -320,6 +381,98 @@ void setup() {
   Serial.println(UDP_PORT);
  
 }
+
+//
+//float asin(float c)
+//{
+//
+//  float out;
+//  out= ((c+(c*c*c)/6+(3*c*c*c*c*c)/40+(5*c*c*c*c*c*c*c)/112+
+//  (35*c*c*c*c*c*c*c*c*c)/1152 +(c*c*c*c*c*c*c*c*c*c*c*0.022)+
+//  (c*c*c*c*c*c*c*c*c*c*c*c*c*.0173)+(c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*.0139)+
+//  (c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*0.0115)+(c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*c*0.01)));
+//  //asin
+//  if(c>=.96 && c<.97){out=1.287+(3.82*(c-.96)); }
+//  if(c>=.97 && c<.98){out=(1.325+4.5*(c-.97));} // arcsin
+//  if(c>=.98 && c<.99){out=(1.37+6*(c-.98));}
+//  if(c>=.99 && c<=1){out=(1.43+14*(c-.99));}  
+//return out;
+//
+//}  //asin()
+
+//float acos(float c){
+//  
+//  float out;
+//  out=asin(sqrt(1-c*c));
+//  return out;
+//} //acos()
+
+void send_UDP_value(float val_to_print)
+{
+UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+UDP.print(val_to_print*100);
+UDP.print('\n');
+UDP.endPacket();
+}
+
+
+void add_val_avr(void)
+{
+  double cos_res = cos(wind_dir * deg_to_rad);
+  double sin_res = sin(wind_dir * deg_to_rad);
+  sum_temperature+=temperature;
+  sum_humidity+=humidity;
+  sum_x_wind=sum_x_wind + cos_res;  //for vector sum calculation
+  sum_y_wind=sum_y_wind + sin_res;  //for vector sum calculation
+  sum_wind_avr+=wind_avr;
+  if (max_wind_gust < wind_gust) 
+     { max_wind_gust = wind_gust; }
+  packet_no++;
+
+  //send_UDP_value(cos_res);
+}
+
+
+
+void prep_avr_val(void)
+{
+  double xy_wind_len = sqrt( sq(sum_x_wind) + sq(sum_y_wind));
+  if (packet_no>0)
+  {
+    avr_temperature = sum_temperature / packet_no;
+    avr_humidity = sum_humidity / packet_no;
+    avr_wind_avr = sum_wind_avr / packet_no;
+    if (xy_wind_len > 0) {
+    avr_wind_dir=(acos(abs(sum_x_wind)/xy_wind_len)*180/3.14);
+    if ((sum_x_wind >= 0) && (sum_y_wind >= 0)) {avr_wind_dir= avr_wind_dir;}   
+    if ((sum_x_wind < 0) && (sum_y_wind > 0)) {avr_wind_dir= 180 - avr_wind_dir;}
+    if ((sum_x_wind <= 0) && (sum_y_wind <= 0)) {avr_wind_dir= 180 + avr_wind_dir;}
+    if ((sum_x_wind > 0) && (sum_y_wind < 0)) {avr_wind_dir= 360 - avr_wind_dir;}
+    avr_wind_consistency = (xy_wind_len / packet_no);
+    }
+    if (xy_wind_len == 0) {avr_wind_dir=0;}
+    
+  }
+
+  //send_UDP_value(sum_x_wind);
+  //send_UDP_value(xy_wind_len);
+  //send_UDP_value(avr_wind_dir);
+  
+  avr_wind_gust = max_wind_gust;
+  //avr_wind_gust = 22.3;
+
+  sum_temperature = 0;
+  sum_humidity = 0;
+  sum_wind_avr = 0;
+  max_wind_gust = 0;
+  sum_x_wind = 0;
+  sum_y_wind = 0;
+  packet_no = 0;
+  last_integr=millis();
+  avr_number++;
+
+}
+
 
 void send_UDP(void)
 {
@@ -352,7 +505,7 @@ void send_UDP(void)
 
 void loop()
 {
-    bool decode_ok;
+    int decode_ok;
 
     // Clear all sensor data
     ws.clearSlots();
@@ -366,10 +519,10 @@ void loop()
    
 
     
-    if (!decode_ok) {
-     // Serial.printf("Sensor timeout\n");
-    } else {
-      print_data();
+    if (decode_ok == DECODE_OK) {
+      
+      lastDecoded=millis();
+      if (Verbose_level>0) { print_data(); }
 
       temperature=ws.sensor[0].w.temp_c;
       humidity=ws.sensor[0].w.humidity;
@@ -378,10 +531,20 @@ void loop()
       wind_gust=ws.sensor[0].w.wind_gust_meter_sec;
       raindrop=ws.sensor[0].w.rain_mm;
       rssi=ws.sensor[0].rssi;
+      
+      add_val_avr();
+
+     // send_UDP_value(wind_dir);
    
     }
+    else {
+          if (Verbose_level>4) { Serial.printf("Sensor timeout\n");}
+         }
 
- 
+  if ((millis()-last_integr) > INT_TIME) {
+      prep_avr_val();
+     }
+
     // Try to receive data from at least one sensor,
     // finish only if data is complete.
     // (Data can be distributed across multiple radio messages.)
@@ -393,16 +556,16 @@ void loop()
     if (packetSize)
   {
     // receive incoming UDP packets
-    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, UDP.remoteIP().toString().c_str(), UDP.remotePort());
+    if (Verbose_level>2) {Serial.printf("Received %d bytes from %s, port %d\n", packetSize, UDP.remoteIP().toString().c_str(), UDP.remotePort());}
     int len = UDP.read(incomingPacket, 255);
     if (len > 0)
     {
       incomingPacket[len] = 0;
       incomingPacket[len-1] = 0;
     }
-    Serial.printf("UDP packet contents: %s\n", incomingPacket);
+    if (Verbose_level>2) {Serial.printf("UDP packet contents: %s\n", incomingPacket);}
      UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
-     
+     last_time=float(int((millis()-lastDecoded)/1000));
      if (String(incomingPacket) == "temp") {UDP.print(ws.sensor[0].w.temp_c);}
      if (String(incomingPacket) == "hum") {UDP.print(ws.sensor[0].w.humidity);}
      if (String(incomingPacket) == "win_dir") {UDP.print(ws.sensor[0].w.wind_direction_deg);}
@@ -410,20 +573,46 @@ void loop()
      if (String(incomingPacket) == "win_gus") {UDP.print(ws.sensor[0].w.wind_gust_meter_sec);}
      if (String(incomingPacket) == "rain") {UDP.print(ws.sensor[0].w.rain_mm);}
      if (String(incomingPacket) == "rssi") {UDP.print(ws.sensor[0].rssi);}
+     if (String(incomingPacket) == "last") {UDP.print(last_time);}
+
+     if (String(incomingPacket) == "atemp") {UDP.print(avr_temperature);}
+     if (String(incomingPacket) == "ahum") {UDP.print(avr_humidity);}
+     if (String(incomingPacket) == "awin_dir") {UDP.print(avr_wind_dir);}
+     if (String(incomingPacket) == "awin_avr") {UDP.print(avr_wind_avr);}
+     if (String(incomingPacket) == "awin_gus") {UDP.print(avr_wind_gust);}
+     if (String(incomingPacket) == "atime") {UDP.print(millis()-last_integr);}
+     if (String(incomingPacket) == "anum") {UDP.print(avr_number);}
+     if (String(incomingPacket) == "apack") {UDP.print(packet_no);}
+     if (String(incomingPacket) == "win_qual") {UDP.print(avr_wind_consistency);}
+
      UDP.print('\n');
      UDP.endPacket();
+    
     // send back a reply, to the IP address and port we got the packet from
     //send_UDP();
   }
 
+ 
+
  if ((millis() - lastTime) > timerDelay && decode_ok == DECODE_OK) {
           // Send Events to the Web Server with the Sensor Readings
     events.send("ping",NULL,millis());
-    events.send(String(temperature,1).c_str(),"temperature",millis());
-    events.send(String(humidity,0).c_str(),"humidity",millis());
-    events.send(String(wind_dir,0).c_str(),"wind_dir",millis());
-    events.send(String(wind_avr,1).c_str(),"wind_avr",millis());
-    events.send(String(wind_gust,1).c_str(),"wind_gust",millis());
+
+    if (avr_val_www) {
+        events.send(String(avr_temperature,1).c_str(),"temperature",millis());
+        events.send(String(avr_humidity,0).c_str(),"humidity",millis());
+        events.send(String(avr_wind_dir,0).c_str(),"wind_dir",millis());
+        events.send(String(avr_wind_avr,1).c_str(),"wind_avr",millis());
+        events.send(String(avr_wind_gust,1).c_str(),"wind_gust",millis());
+    }
+    else
+    {
+        events.send(String(temperature,1).c_str(),"temperature",millis());
+        events.send(String(humidity,0).c_str(),"humidity",millis());
+        events.send(String(wind_dir,0).c_str(),"wind_dir",millis());
+        events.send(String(wind_avr,1).c_str(),"wind_avr",millis());
+        events.send(String(wind_gust,1).c_str(),"wind_gust",millis());
+    }
     events.send(String(raindrop,1).c_str(),"raindrop",millis());
     events.send(String(rssi,0).c_str(),"rssi",millis());
     
